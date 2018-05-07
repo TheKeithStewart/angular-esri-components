5db6a1a273c8166ac57c90bba08f1e64cc0ddacc
Keith Stewart <kgstewart@gmail.com>
2018-05-07 10:38:58 -0500

    #25: remove loadScript call because it is happening during loadModules

--- projects/angular-esri-components/src/environments/environment.ts
+++ projects/angular-esri-components/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  arcGisScriptUrl: 'https://js.arcgis.com/4.7/',
   arcGisCssUrl: 'http://js.arcgis.com/4.7/esri/css/main.css'
 };
--- projects/angular-esri-components/src/lib/core/esri-module-provider.ts
+++ projects/angular-esri-components/src/lib/core/esri-module-provider.ts
@@ -1,17 +1,11 @@
 import { Injectable, ElementRef, EventEmitter } from '@angular/core';
-import { loadModules, loadScript } from 'esri-loader';
-import { environment } from '../../environments/environment';
+import { loadModules } from 'esri-loader';
 
 @Injectable()
 export class EsriModuleProvider {
   constructor() {}
 
   require(moduleNames: string[]): Promise<any[]> {
-    return loadScript({
-      // the specific version of the API that is to be used
-      url: environment.arcGisScriptUrl
-    }).then(() => {
-      return loadModules(moduleNames);
-    });
+    return loadModules(moduleNames);
   }
 }
