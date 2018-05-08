a0b3b6110706058831391f88130faa099da2213d
Keith Stewart <kgstewart@gmail.com>
2018-05-08 05:26:36 -0500

    refactor to extract some common functionality into separate function

--- projects/angular-esri-components/src/lib/core/esri-map.service.ts
+++ projects/angular-esri-components/src/lib/core/esri-map.service.ts
@@ -7,71 +7,110 @@ export class EsriMapService {
   map: __esri.Map;
   view: __esri.View;
 
-  constructor(private moduleProvider: EsriModuleProvider) { }
+  constructor(private moduleProvider: EsriModuleProvider) {}
 
-  loadMap(mapProperties: __esri.MapProperties, mapViewProperties: __esri.ViewProperties, mapEl: ElementRef, viewType: string = 'MapView') {
-    return this.moduleProvider.require(['esri/Map', 'esri/views/' + viewType])
-      .then(([Map, View]: [__esri.MapConstructor, __esri.MapViewConstructor]) => {
-        // create map
-        const map = new Map(mapProperties);
+  prepareViewProps(
+    mapViewProperties: __esri.ViewProperties,
+    mapEl: ElementRef<any>,
+    map: __esri.Map
+  ) {
+    const newViewProps = this.extend({}, mapViewProperties);
 
-        // prepare properties that should be set locally
-        // create a new object so as to not modify the provided object
-        const newViewProps = this.extend({}, mapViewProperties);
-        if (!newViewProps.container) { newViewProps.container = mapEl.nativeElement.id; }
-        if (!newViewProps.map) {
-          newViewProps.map = map;
-        }
+    if (!newViewProps.container) {
+      newViewProps.container = mapEl.nativeElement.id;
+    }
+    if (!newViewProps.map) {
+      newViewProps.map = map;
+    }
 
-        // create the MapView
-        const view = new View(newViewProps);
-
-        this.map = map;
-        this.view = view;
-
-        this.isLoaded.emit();
-
-        return {
-          map: map,
-          view: view
-        };
-      });
+    return newViewProps;
   }
 
-  loadWebMap(webMapProperties: __esri.WebMapProperties, viewProperties: __esri.ViewProperties, mapEl: ElementRef,
-    viewType: string = 'MapView') {
-    return this.moduleProvider.require(['esri/views/' + viewType, 'esri/WebMap'])
-    .then(([View, WebMap]: [__esri.ViewConstructor, __esri.WebMapConstructor]) => {
-        // create map
-        const map = new WebMap(webMapProperties);
+  loadMap(
+    mapProperties: __esri.MapProperties,
+    viewProperties: __esri.ViewProperties,
+    mapEl: ElementRef,
+    viewType: string = 'MapView'
+  ) {
+    return this.moduleProvider
+      .require(['esri/Map', 'esri/views/' + viewType])
+      .then(
+        ([Map, View]: [__esri.MapConstructor, __esri.ViewConstructor]) => {
+          // create map
+          const map = new Map(mapProperties);
 
-        // prepare properties that should be set locally
-        // create a new object so as to not modify the provided object
-        const newViewProps = this.extend({}, viewProperties);
-        if (!newViewProps.container) { newViewProps.container = mapEl.nativeElement.id; }
-        if (!newViewProps.map) { newViewProps.map = map; }
+          // prepare properties that should be set locally
+          // create a new object so as to not modify the provided object
+          const newViewProps = this.prepareViewProps(
+            viewProperties,
+            mapEl,
+            map
+          );
 
-        // create the MapView
-        const view = new View(newViewProps);
+          // create the MapView
+          const view = new View(newViewProps);
 
-        this.map = map;
-        this.view = view;
+          this.map = map;
+          this.view = view;
 
-        this.isLoaded.emit();
+          this.isLoaded.emit();
 
-        return {
-          map: map,
-          view: view
-        };
-    });
+          return {
+            map: map,
+            view: view
+          };
+        }
+      );
+  }
+
+  loadWebMap(
+    webMapProperties: __esri.WebMapProperties,
+    viewProperties: __esri.ViewProperties,
+    mapEl: ElementRef,
+    viewType: string = 'MapView'
+  ) {
+    return this.moduleProvider
+      .require(['esri/WebMap', 'esri/views/' + viewType])
+      .then(
+        ([WebMap, View]: [
+          __esri.WebMapConstructor,
+          __esri.ViewConstructor
+        ]) => {
+          // create map
+          const map = new WebMap(webMapProperties);
+
+          // prepare properties that should be set locally
+          // create a new object so as to not modify the provided object
+          const newViewProps = this.prepareViewProps(
+            viewProperties,
+            mapEl,
+            map
+          );
+
+          // create the MapView
+          const view = new View(newViewProps);
+
+          this.map = map;
+          this.view = view;
+
+          this.isLoaded.emit();
+
+          return {
+            map: map,
+            view: view
+          };
+        }
+      );
   }
 
   addWidget(element: HTMLElement, position: string) {
     this.view.ui.add(element, position);
   }
 
-  extend(obj: any, src: any) {
-    Object.keys(src).forEach(function (key) { obj[key] = src[key]; });
+  extend(obj: __esri.ViewProperties, src: __esri.ViewProperties) {
+    Object.keys(src).forEach(key => {
+      obj[key] = src[key];
+    });
     return obj;
   }
 }
