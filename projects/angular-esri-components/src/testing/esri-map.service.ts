96386afa3fd20fc21c0408b000df2ac0f4aac7b5
Keith Stewart <kgstewart@gmail.com>
2018-05-08 06:19:11 -0500

    #2: add unit tests for map component

--- projects/angular-esri-components/src/testing/esri-map.service.ts
+++ projects/angular-esri-components/src/testing/esri-map.service.ts
@@ -3,4 +3,6 @@ import { of } from 'rxjs';
 export class MockEsriMapSerivice {
   isLoaded = of(true);
   addWidget = jasmine.createSpy('addWidget');
+  loadMap = jasmine.createSpy('loadMap');
+  loadWebMap = jasmine.createSpy('loadWebMap');
 }
