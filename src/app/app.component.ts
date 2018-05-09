98dac98002e5224750f16301cee736ee97ca2272
Keith Stewart <kgstewart@gmail.com>
2018-05-09 05:36:46 -0500

    #26: add a layer with sublayers to example app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,39 @@ export class AppComponent {
   onMapInit(mapInfo: {map: __esri.Map, mapView: __esri.MapView}) {
     this.map = mapInfo.map;
     this.mapView = mapInfo.mapView;
+
+    // add a layer with sublayers to map
+    this.moduleProvider
+      .require(['esri/layers/MapImageLayer'])
+      .then(
+        ([MapImageLayer]) => {
+          const layer = new MapImageLayer({
+            url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer',
+            sublayers: [
+              {
+                id: 3,
+                title: 'States',
+                visible: false
+              },
+              {
+                id: 2,
+                title: 'Railroads',
+                visible: true
+              },
+              {
+                id: 1,
+                title: 'Highways',
+                visible: true
+              },
+              {
+                id: 0,
+                title: 'Cities',
+                visible: true
+              }
+            ]
+          });
+
+          this.map.layers.add(layer);
+        });
   }
 }
